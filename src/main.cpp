d07c01609bc75bdcc3100420118c1566756bb0ad
Martin Schilling <martin.schilling@insecia.com>
2017-10-31 19:20:06 +0100

    Added some error handling and non-blocking stuff for testing

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,9 @@ namespace f = ::arangodb::fuerte;
 namespace fu = ::arangodb::fuerte;
 namespace vp = ::arangodb::velocypack;
 
+//void createCollection(std::string collectionName, int timeout);
+
+
 Php::Value vpack(Php::Parameters &params)
 {
     // there is one input array, cast the PHP variable to a vector of ints
@@ -54,29 +57,48 @@ Php::Value vpack(Php::Parameters &params)
     std::cout << vp::valueTypeName(vp::ValueType::Object) << std::endl;
 
     std::cout << std::endl << std::endl;
+    //createCollection("testobi", 3);
 
-
-
-    f::ConnectionBuilder cbuilder;
-    cbuilder.host("vst://arangodb:8529");
-
-    std::unique_ptr<f::EventLoopService> eventLoopService = std::unique_ptr<f::EventLoopService>(new f::EventLoopService(1));
-    std::shared_ptr<f::Connection> connection = cbuilder.connect(*eventLoopService);
-
-    VPackBuilder builder;
-    builder.openObject();
-    builder.add("name" , VPackValue("testobi"));
-    builder.close();
-
-    auto request = fu::createRequest(fu::RestVerb::Post, "/_api/collection");
-    request->addVPack(builder.slice());
-    auto result = connection->sendRequest(std::move(request));
-
-    std::cout << result->statusCode() << std::endl;
     return input;
 }
 
 
+Php::Value createCollection(Php::Parameters &params)
+{
+    std::string collectionName = params[0];
+    int timeout = params[1];
+
+    std::shared_ptr<f::Connection> connection;
+    std::unique_ptr<f::EventLoopService> eventLoopService = std::unique_ptr<f::EventLoopService>(new f::EventLoopService(1));
+
+    f::WaitGroup wg;
+    wg.add();
+
+    f::ConnectionBuilder cbuilder;
+    cbuilder.host("vst://arangodb:8529");
+    cbuilder.onFailure([&](f::Error errorCode, const std::string& errorMessage){  });
+    connection = cbuilder.connect(*eventLoopService);
+
+    VPackBuilder builder;
+    builder.openObject();
+    builder.add("name" , VPackValue(collectionName));
+    builder.close();
+
+    auto request = fu::createRequest(fu::RestVerb::Post, "/_api/collection");
+    request->addVPack(builder.slice());
+    connection->sendRequest(std::move(request),[&](f::Error, std::unique_ptr<f::Request>, std::unique_ptr<f::Response> response){
+        std::cout << response->statusCode() << std::endl;
+        wg.done();
+    });
+
+
+    auto success = wg.wait_for(std::chrono::seconds(timeout));
+    if(!success) {
+        throw Php::Exception("Could not connect to arangodb");
+    }
+
+    return true;
+}
 
 
 
@@ -109,6 +131,11 @@ extern "C" {
             Php::ByVal("input", Php::Type::Array)
         });
 
+
+        extension.add<createCollection>("createCollection", {
+            Php::ByVal("collectionName", Php::Type::String),
+            Php::ByVal("timeout", Php::Type::Numeric)
+        });
 
         // return the extension
         return extension;
--- test.php
+++ test.php
@@ -4,13 +4,22 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
 // fill an array with random numbers
- $count = 10000;
- $x = array();
- for ($i=0; $i<$count; $i++) $x[] = rand(0, 1000000);
+$count = 10000;
+$x = array();
+for ($i=0; $i<$count; $i++) $x[] = rand(0, 1000000);
 
 // // run the native and scripted bubblesort functions
 // $start = microtime(true);
- $y = vpack($x);
+$y = vpack($x);
+
+$t1 = microtime(true);
+try {
+    createCollection("Testobi", 2);
+} catch(Exception $e) {
+    echo "Creating arangodb collection failed: " . $e->getMessage() . PHP_EOL;
+}
+$t2 = microtime(true);
+echo "Took " . ($t2 - $t1) . " seconds\n";
 // $native = microtime(true);
 
 // // show the results
