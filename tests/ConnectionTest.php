b831922e37fab074bf06954e41acf961b1f18744
Martin Schilling <martin.schilling@insecia.com>
2017-11-04 16:35:36 +0100

    Moved Vpack tests from ConnectionTest.php to VpackTest.php

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -50,42 +50,4 @@ class ConnectionTest extends TestCase
 
         $connection->connect();
     }
-
-    /**
-     * @test
-     */
-    public function it_creates_vpack_from_array(): void
-    {
-        echo PHP_EOL;
-
-        $vpack = Vpack::fromArray(["b"]);
-        echo $vpack->toHex() . PHP_EOL;
-
-        $vpack = Vpack::fromArray(["a" => "b"]);
-        echo $vpack->toHex() . PHP_EOL;
-
-        $vpack = Vpack::fromJson("{\"0\": \"b\"}");
-        echo $vpack->toHex() . PHP_EOL;
-
-        $vpack = Vpack::fromJson("{\"a\": \"b\"}");
-        echo $vpack->toHex() . PHP_EOL;
-
-        $vpack = new Vpack();
-        echo $vpack->toHex() . PHP_EOL;
-
-        $this->assertTrue(true);
-    }
-
-
-    /**
-     * @test
-     */
-    public function it_fails_creating_vpack_from_invalid_json(): void
-    {
-        $this->expectException(\Exception::class);
-
-        $vpack = new Vpack();
-        $vpack->fromJson("{a:\"b\"}");
-        echo $vpack->toHex();
-    }
 }
