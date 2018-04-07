6d76009dbe0977e37c6847df943e28a3543d5d86
Martin Schilling <martin.schilling@insecia.com>
2018-04-07 12:43:55 +0200

    Attempt to fix request test segfaults

--- php_request.h
+++ php_request.h
@@ -28,9 +28,9 @@ namespace {
         }
 
         auto intern = Z_OBJECT_REQUEST_P(getThis());;
-/*
+
         intern->set_http_method(method);
-        intern->set_path(std::string(path, path_length));
+/*        intern->set_path(std::string(path, path_length));
 /*
         if(Z_TYPE_P(vpack_value) == IS_STRING) {
             intern->set_vpack_from_json(Z_STRVAL_P(vpack_value));
